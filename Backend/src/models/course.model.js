89870e72ed4b6011deaabda68b928becd2b409ee
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-04 02:46:27 +0530

    Add course model

---
+++ Backend/src/models/course.model.js
@@ -0,0 +1,22 @@
+import { model, Schema } from "mongoose";
+
+const courseSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        modules: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Module",
+            },
+        ],
+    },
+    { timestamps: true },
+);
+
+export const Course = model("Course", courseSchema);
