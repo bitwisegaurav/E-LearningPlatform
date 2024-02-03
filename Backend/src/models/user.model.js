44be2c5228cc1ec14edab27caddd6dd82d8e83f9
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-04 02:03:33 +0530

    Remove unique constraint from email field in user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -14,7 +14,6 @@ const userSchema = new Schema(
         email: {
             type: String,
             required: true,
-            unique: true,
         },
         articleCount: {
             type: Number,
