31b783dd1e56c34d02d4bd45b420171f3f8b95e6
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-04 02:07:06 +0530

    Fix reference to "Questions" model in user schema

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -42,7 +42,7 @@ const userSchema = new Schema(
         solvedQuestions: [
             {
                 type: Schema.Types.ObjectId,
-                ref: "Questions",
+                ref: "Question",
             },
         ],
     },
