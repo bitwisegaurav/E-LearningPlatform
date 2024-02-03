8534c1b7bc287e8f1c9cfe7000ef94169ccd76f5
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-04 01:57:06 +0530

    Add user model to Backend

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,53 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        articleCount: {
+            type: Number,
+            default: 0,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePic: {
+            type: String, // url of th e image
+        },
+        coverImage: {
+            type: String, // url of the image
+        },
+        progress: {
+            type: Object,
+            default: {},
+        },
+        courses: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Course",
+            },
+        ],
+        solvedQuestions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Questions",
+            },
+        ],
+    },
+    { timestamps: true },
+);
+
+export const User = model("User", userSchema);
