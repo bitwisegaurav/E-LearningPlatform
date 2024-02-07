5af3ed2fbd6097293157b027eff6c67b027d3852
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-08 04:18:39 +0530

    Add cloudinary.util.js for image upload and deletion

---
+++ Backend/src/utils/cloudinary.util.js
@@ -0,0 +1,35 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImage = async (localFilePath) => {
+    if (!localFilePath) return null;
+    try {
+        const cloudinaryUploadValue = await cloudinary.uploader.upload(
+            localFilePath,
+            { resource_type: "auto" },
+        );
+        fs.unlinkSync(localFilePath);
+        return cloudinaryUploadValue;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log(error);
+        return null;
+    }
+};
+
+const deleteImage = async (publicIds) => {
+    try {
+        await cloudinary.uploader.destroy(publicIds);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+export { uploadImage, deleteImage };
