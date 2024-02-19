89b49fd2bf0b22100b9bfe4a6a6b4bf767716f94
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-20 04:01:02 +0530

    Add user controller with CRUD operations

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,190 @@
+import { User } from '../models/user.model.js';
+import { ApiResponse } from '../utils/ApiResponse.util.js';
+import { ApiError } from '../utils/ApiError.util.js';
+import { uploadImage, deleteImage } from '../utils/cloudinary.util.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { asyncHandler } from '../utils/asyncHandler.util.js';
+
+const options = {
+    httpOnly: true,
+    secure: true,
+};
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating acess and refresh token",
+        );
+    }
+};
+
+const registerUserProfile = asyncHandler(async (req, res, next) => {
+    const { username, name, email, password } = req.body;
+
+    // validation for fields
+    if ([username, email, name, password].some(
+        (field) => field?.trim() === "",
+    )) {
+        throw new ApiError(400, 'Please provide all the required fields');
+    }
+
+    // check if user is already exists
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if (existedUser) {
+        throw new ApiError(409, "User with username or email already exists");
+    }
+
+    // check for avatar and coverImage localpaths
+
+    const avatarLocalPath = req.files?.avatar ? req.files?.avatar[0]?.path : null;
+    const coverImageLocalPath = req.files?.coverImage ? req.files?.coverImage[0]?.path : null;
+
+    if(!avatarLocalPath || !coverImageLocalPath) {
+        throw new ApiError(400, 'Please provide avatar and coverImage');
+    }
+
+    // upload images to cloudinary
+    const avatarCloudinaryResponse = await uploadImage(avatarLocalPath);
+    const coverImageCloudinaryResponse = await uploadImage(coverImageLocalPath);
+
+    if(!avatarCloudinaryResponse || !coverImageCloudinaryResponse) {
+        throw new ApiError(500, 'Error while uploading images to cloudinary');
+    }
+
+    // create user
+    const user = await User.create({
+        username: username.toLowerCase(),
+        name,
+        email,
+        password,
+        profilePic: avatarCloudinaryResponse.url,
+        coverImage: coverImageCloudinaryResponse.url,
+    }).select('-password');
+
+    // check if user is created or not
+    if (!user) {
+        throw new ApiError(500, 'Error while creating user');
+    }
+
+    // generate access and refresh token
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user?._id);
+
+    return res.status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json({
+        message: 'User created successfully',
+        data: {
+            user,
+        },
+    });
+})
+
+const getUserProfile = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id).select('-password');
+    if (!user) {
+      throw new ApiError(404, 'User not found');
+    }
+
+    const responseData = {
+      user,
+    };
+
+    return res.status(200).json(new ApiResponse(200, responseData));
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw new ApiError(404, 'User not found');
+    }
+
+    // Update user data based on the request body
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    // Handle profile picture update
+    if (req.file) {
+      const cloudinaryResponse = await uploadImage(req.file.path);
+      if (cloudinaryResponse) {
+        user.profilePic = cloudinaryResponse.secure_url;
+        // Delete previous profile picture from Cloudinary
+        await deleteImage(user.profilePic);
+      }
+    }
+
+    // Save the updated user data
+    await user.save();
+
+    const responseData = {
+      user,
+      message: 'Profile updated successfully',
+    };
+
+    return res.status(200).json(new ApiResponse(200, responseData));
+});
+
+const changeUserPassword = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw new ApiError(404, 'User not found');
+    }
+
+    const {password, newPassword} = req.body;
+
+    if(!password || !newPassword) {
+        throw new ApiError(400, 'Please provide new password');
+    }
+
+    // Check if the provided current password is correct
+    const isCurrentPasswordCorrect = await user.isPasswordCorrect(password);
+    if (!isCurrentPasswordCorrect) {
+      throw new ApiError(400, 'Current password is incorrect');
+    }
+
+    // Update the password
+    user.password = newPassword;
+
+    // Save the updated user data
+    await user.save();
+
+    const responseData = {
+      message: 'Password changed successfully',
+    };
+
+    return res.status(200).json(new ApiResponse(200, responseData));
+});
+
+const deleteUserAccount = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user?._id);
+    if (!user) {
+      throw new ApiError(404, 'User not found');
+    }
+
+    // Delete the user from the database
+    await user.remove();
+
+    const responseData = {
+      message: 'Account deleted successfully',
+    };
+
+    return res.status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, responseData));
+});
+
+export { registerUserProfile, getUserProfile, updateUserProfile, changeUserPassword, deleteUserAccount };
