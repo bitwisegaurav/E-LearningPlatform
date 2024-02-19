3f8a6ad4cb94bdcf33bd6d0394ccd2d66b6c8b36
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-20 04:01:08 +0530

    Add registerUser route and controller

--- Backend/src/routers/user.routes.js
+++ Backend/src/routers/user.routes.js
@@ -1,12 +1,20 @@
 import Router from "express";
+import { registerUserProfile } from "../controllers/user.controller";
 
 const router = Router();
 
-router.route('/health').get((__, res) => {
+router.route("/health").get((__, res) => {
     res.json({
         message: "Serving users data perfectly",
-        status: 200
-    })
-})
+        status: 200,
+    });
+});
+router.route("/registerUser").post(
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 },
+    ]),
+    registerUserProfile,
+);
 
 export default router;
