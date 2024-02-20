39bad9921f59ab2a158e66aa4a2b9734d5c60b91
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-02-21 00:45:07 +0530

    Refactor user.controller.js file

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,9 +1,8 @@
-import { User } from '../models/user.model.js';
-import { ApiResponse } from '../utils/ApiResponse.util.js';
-import { ApiError } from '../utils/ApiError.util.js';
-import { uploadImage, deleteImage } from '../utils/cloudinary.util.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
-import { asyncHandler } from '../utils/asyncHandler.util.js';
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.util.js";
+import { ApiError } from "../utils/ApiError.util.js";
+import { uploadImage, deleteImage } from "../utils/cloudinary.util.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
 
 const options = {
     httpOnly: true,
@@ -32,10 +31,10 @@ const registerUserProfile = asyncHandler(async (req, res, next) => {
     const { username, name, email, password } = req.body;
 
     // validation for fields
-    if ([username, email, name, password].some(
-        (field) => field?.trim() === "",
-    )) {
-        throw new ApiError(400, 'Please provide all the required fields');
+    if (
+        [username, email, name, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "Please provide all the required fields");
     }
 
     // check if user is already exists
@@ -49,19 +48,23 @@ const registerUserProfile = asyncHandler(async (req, res, next) => {
 
     // check for avatar and coverImage localpaths
 
-    const avatarLocalPath = req.files?.avatar ? req.files?.avatar[0]?.path : null;
-    const coverImageLocalPath = req.files?.coverImage ? req.files?.coverImage[0]?.path : null;
+    const avatarLocalPath = req.files?.avatar
+        ? req.files?.avatar[0]?.path
+        : null;
+    const coverImageLocalPath = req.files?.coverImage
+        ? req.files?.coverImage[0]?.path
+        : null;
 
-    if(!avatarLocalPath || !coverImageLocalPath) {
-        throw new ApiError(400, 'Please provide avatar and coverImage');
+    if (!avatarLocalPath || !coverImageLocalPath) {
+        throw new ApiError(400, "Please provide avatar and coverImage");
     }
 
     // upload images to cloudinary
     const avatarCloudinaryResponse = await uploadImage(avatarLocalPath);
     const coverImageCloudinaryResponse = await uploadImage(coverImageLocalPath);
 
-    if(!avatarCloudinaryResponse || !coverImageCloudinaryResponse) {
-        throw new ApiError(500, 'Error while uploading images to cloudinary');
+    if (!avatarCloudinaryResponse || !coverImageCloudinaryResponse) {
+        throw new ApiError(500, "Error while uploading images to cloudinary");
     }
 
     // create user
@@ -72,87 +75,85 @@ const registerUserProfile = asyncHandler(async (req, res, next) => {
         password,
         profilePic: avatarCloudinaryResponse.url,
         coverImage: coverImageCloudinaryResponse.url,
-    }).select('-password');
+    }).select("-password");
 
     // check if user is created or not
     if (!user) {
-        throw new ApiError(500, 'Error while creating user');
+        throw new ApiError(500, "Error while creating user");
     }
 
     // generate access and refresh token
-    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user?._id);
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+        user?._id,
+    );
 
-    return res.status(201)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json({
-        message: 'User created successfully',
-        data: {
-            user,
-        },
-    });
-})
+    return res
+        .status(201)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json({
+            message: "User created successfully",
+            data: {
+                user,
+            },
+        });
+});
 
 const getUserProfile = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user._id).select('-password');
+    const user = await User.findById(req.user._id).select("-password");
     if (!user) {
-      throw new ApiError(404, 'User not found');
+        throw new ApiError(404, "User not found");
     }
 
     const responseData = {
-      user,
+        user,
     };
 
     return res.status(200).json(new ApiResponse(200, responseData));
+});
+
+const getUserProfileByUsername = asyncHandler(async (req, res) => {
+    const { username } = req.params;
+
+    const user = await User.find({ username }).select(
+        "-password -refreshToken",
+    );
+
+    const responseData = {
+        user,
+    };
+
+    res.status(200).json(new ApiResponse(200, responseData));
 });
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      throw new ApiError(404, 'User not found');
-    }
+    const { username, name, email } = req.body;
 
-    // Update user data based on the request body
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-
-    // Handle profile picture update
-    if (req.file) {
-      const cloudinaryResponse = await uploadImage(req.file.path);
-      if (cloudinaryResponse) {
-        user.profilePic = cloudinaryResponse.secure_url;
-        // Delete previous profile picture from Cloudinary
-        await deleteImage(user.profilePic);
-      }
-    }
-
-    // Save the updated user data
-    await user.save();
+    const user = await User.findById(req.user?._id).select(
+        "-password -refreshToken",
+    );
 
     const responseData = {
-      user,
-      message: 'Profile updated successfully',
+        user,
+        message: "Profile updated successfully",
     };
 
-    return res.status(200).json(new ApiResponse(200, responseData));
+    res.status(200).json(new ApiResponse(200, responseData));
 });
 
 const changeUserPassword = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      throw new ApiError(404, 'User not found');
+    const { password, newPassword } = req.body;
+    
+    if (!password || !newPassword) {
+      throw new ApiError(400, "Please provide new password");
     }
-
-    const {password, newPassword} = req.body;
-
-    if(!password || !newPassword) {
-        throw new ApiError(400, 'Please provide new password');
-    }
-
+    
+    const user = req.user;
+  
     // Check if the provided current password is correct
     const isCurrentPasswordCorrect = await user.isPasswordCorrect(password);
     if (!isCurrentPasswordCorrect) {
-      throw new ApiError(400, 'Current password is incorrect');
+        throw new ApiError(400, "Current password is incorrect");
     }
 
     // Update the password
@@ -162,29 +163,34 @@ const changeUserPassword = asyncHandler(async (req, res) => {
     await user.save();
 
     const responseData = {
-      message: 'Password changed successfully',
+        message: "Password changed successfully",
     };
 
     return res.status(200).json(new ApiResponse(200, responseData));
 });
 
 const deleteUserAccount = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user?._id);
-    if (!user) {
-      throw new ApiError(404, 'User not found');
-    }
+    const user = req.user;
 
     // Delete the user from the database
     await user.remove();
 
     const responseData = {
-      message: 'Account deleted successfully',
+        message: "Account deleted successfully",
     };
 
-    return res.status(200)
+    return res
+        .status(200)
         .clearCookie("accessToken", options)
         .clearCookie("refreshToken", options)
         .json(new ApiResponse(200, responseData));
 });
 
-export { registerUserProfile, getUserProfile, updateUserProfile, changeUserPassword, deleteUserAccount };
+export {
+    registerUserProfile,
+    getUserProfile,
+    getUserProfileByUsername,
+    updateUserProfile,
+    changeUserPassword,
+    deleteUserAccount,
+};
