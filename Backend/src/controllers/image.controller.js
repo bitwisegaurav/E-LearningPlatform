d8602ae51bbb49a42156e17c5a5c0df566b37b09
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-01 22:54:30 +0530

    Add image upload handler

---
+++ Backend/src/controllers/image.controller.js
@@ -0,0 +1,26 @@
+import { ApiError } from "../utils/ApiError.util";
+import { ApiResponse } from "../utils/ApiResponse.util";
+import { asyncHandler } from "../utils/asyncHandler.util";
+
+const uploadImageHandler = asyncHandler(async (req, res) => {
+    const imageLocalPath = req.file?.path;
+
+    if (!imageLocalPath || imageLocalPath.trim() === "") {
+        throw new ApiError(400, "Image is required");
+    }
+
+    const image = await uploadImage(imageLocalPath);
+
+    if (!image) {
+        throw new ApiError(500, "Failed to upload image");
+    }
+
+    return res.status(201).json(
+        new ApiResponse({
+            data: image.url,
+            message: "Image uploaded successfully",
+        }),
+    );
+});
+
+export { uploadImageHandler };
