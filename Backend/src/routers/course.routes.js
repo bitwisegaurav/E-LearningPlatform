46d0c559266ea3253a1e57e0af3e8116c4307f56
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-02 17:19:21 +0530

    Add routes and controllers for course management

--- Backend/src/routers/course.routes.js
+++ Backend/src/routers/course.routes.js
@@ -1,4 +1,16 @@
 import Router from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyAdmin } from "../middlewares/adminAuth.middleware";
+import { upload } from "../middlewares/multer.middleware";
+import {
+    createCourse,
+    getCourses,
+    getCourseById,
+    getModules,
+    updateCourse,
+    updateCourseImage,
+    deleteCourse,
+} from "../controllers/course.controller";
 
 const router = Router();
 
@@ -8,5 +20,21 @@ router.route("/health").get((__, res) => {
         status: 200,
     });
 });
+
+/**
+ * export { createCourse, getCourses, getCourseById, getModules, updateCourse, deleteCourse };
+ */
+
+router
+    .route("/create-course")
+    .post(verifyJWT, verifyAdmin, upload.single("image"), createCourse);
+router.route("/get-courses").get(verifyJWT, getCourses);
+router.route("/get-course/:id").get(verifyJWT, getCourseById);
+router.route("/get-modules").get(verifyJWT, getModules);
+router.route("/update-course-details/:id").patch(verifyJWT, verifyAdmin, updateCourse);
+router
+    .route("/update-course-image/:id")
+    .patch(verifyJWT, verifyAdmin, upload.single("image"), updateCourseImage);
+router.route("/delete-course/:id").delete(verifyJWT, verifyAdmin, deleteCourse);
 
 export default router;
