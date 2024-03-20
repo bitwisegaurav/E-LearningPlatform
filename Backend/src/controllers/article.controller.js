a0ee2eb1060ad1f575dd4bdd9144512030bce6f9
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-20 16:26:42 +0530

    fix naming error and added like functionality

--- Backend/src/controllers/article.controller.js
+++ Backend/src/controllers/article.controller.js
@@ -1,112 +1,205 @@
-import { Articles } from "../models/articles.model.js";
+import { Article } from "../models/article.model.js";
 import { ApiResponse } from "../utils/apiResponse.util.js";
 import { ApiError } from "../utils/apiError.util.js";
 import { asyncHandler } from "../utils/asyncHandler.util.js";
 import { deleteImage } from "../utils/cloudinary.util.js";
 
 const createArticle = asyncHandler(async (req, res) => {
-  const { title, body, imageURL } = req.body;
+    const { title, body, imageURL } = req.body;
 
-  if ([title, body, imageURL].some((field) => field?.trim() === "")) {
-    throw new ApiError(400, "Title and body are required");
-  }
+    if ([title, body, imageURL].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "Title and body are required");
+    }
 
-  const article = await Articles.create({
-    title,
-    body,
-    imageURL,
-    owner: req.user?._id,
-  });
+    const article = await Article.create({
+        title,
+        body,
+        imageURL,
+        owner: req.user?._id,
+    });
 
-  return res.status(201).json(new ApiResponse(201, article, "Article created successfully"));
+    return res
+        .status(201)
+        .json(new ApiResponse(201, article, "Article created successfully"));
 });
 
 const getArticles = asyncHandler(async (req, res) => {
-  const articles = await Articles.find().populate("owner", "username name avatar");
+    const articles = await Article.aggregate([
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
 
-  return res.status(200).json(new ApiResponse(200, articles, "Articles fetched successfully"));
+                pipeline: {
+                    $project: {
+                        _id: 1,
+                        username: 1,
+                        name: 1,
+                        avatar: 1,
+                    },
+                },
+            },
+        },
+        {
+            $addFields: {
+                owner: {
+                    $arrayElemAt: ["$owner", 0],
+                },
+                likesCount: {
+                    $size: "$likes",
+                },
+            },
+        },
+        {
+            $project: {
+                likes: 0,
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, articles, "Article fetched successfully"));
 });
 
 const getArticleById = asyncHandler(async (req, res) => {
-  const id = req.params?.id || req.body?.id;
+    const id = req.params?.id || req.body?.id;
 
-  if (!id) {
-    throw new ApiError(400, "Article id is required");
-  }
+    if (!id) {
+        throw new ApiError(400, "Article id is required");
+    }
 
-  const article = await Articles.findById(id).populate("owner", "username name avatar");
+    const article = await Article.findById(id).populate(
+        "owner",
+        "username name avatar",
+    );
 
-  if (!article) {
-    throw new ApiError(404, "Article not found");
-  }
+    if (!article) {
+        throw new ApiError(404, "Article not found");
+    }
 
-  return res.status(200).json(new ApiResponse(200, article, "Article fetched successfully"));
+    // add likesCount field and remove likes field
+    article.likesCount = article.likes.length;
+    delete article.likes;
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, article, "Article fetched successfully"));
+});
+
+const likeArticle = asyncHandler(async (req, res) => {
+    const id = req.params?.id || req.body?.id;
+
+    if (!id) {
+        throw new ApiError(400, "Article id is required");
+    }
+
+    const article = await Article.findById(id);
+
+    if (!article) {
+        throw new ApiError(404, "Article not found");
+    }
+
+    article.likes.push(req.user?._id);
+    try {
+        await article.save();
+    } catch (error) {
+        throw new ApiError(500, "Failed to like article");
+    }
+
+    article.likesCount = article.likes.length;
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, article, "Article liked successfully"));
 });
 
 const updateArticle = asyncHandler(async (req, res) => {
-  const id = req.params?.id || req.body?.id;
-  const { title, body, imageURL } = req.body;
+    const id = req.params?.id || req.body?.id;
+    const { title, body, imageURL } = req.body;
 
-  if (!title && !body && !imageURL) {
-    throw new ApiError(400, "Provide atleast one detail of the article to update");
-  }
-
-  if(imageURL?.trim().length) {    
-    const article = await Articles.findById(id);
-    if (!article && !article.imageURL) {
-      throw new ApiError(404, "Article or image is not found");
+    if (!title && !body && !imageURL) {
+        throw new ApiError(
+            400,
+            "Provide atleast one detail of the article to update",
+        );
     }
-    
-    // delete previous image
-    const previousImage = article.imageURL.split("/").pop().split(".")[0];
-    const isDeleted = await deleteImage(previousImage);
-    if (!isDeleted) {
-      throw new ApiError(500, "Failed to delete previous image");
+
+    if (imageURL?.trim().length) {
+        const article = await Article.findById(id);
+        if (!article && !article.imageURL) {
+            throw new ApiError(404, "Article or image is not found");
+        }
+
+        // delete previous image
+        const previousImage = article.imageURL.split("/").pop().split(".")[0];
+        const isDeleted = await deleteImage(previousImage);
+        if (!isDeleted) {
+            throw new ApiError(500, "Failed to delete previous image");
+        }
     }
-  }
 
-  const updatedArticle = await Articles.findByIdAndUpdate(
-    id,
-    {
-      $set: {
-        ...(title && { title }),
-        ...(body && { body }),
-        ...(imageURL && { imageURL }),
-      },
-    },
-    { new: true }
-  );
+    const updatedArticle = await Article.findByIdAndUpdate(
+        id,
+        {
+            $set: {
+                ...(title && { title }),
+                ...(body && { body }),
+                ...(imageURL && { imageURL }),
+            },
+        },
+        { new: true },
+    );
 
-  if (!updatedArticle) {
-    throw new ApiError(404, "Some error occured while updating article");
-  }
+    if (!updatedArticle) {
+        throw new ApiError(404, "Some error occured while updating article");
+    }
 
-  return res.status(200).json(new ApiResponse(200, updatedArticle, "Article updated successfully"));
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updatedArticle,
+                "Article updated successfully",
+            ),
+        );
 });
 
 const deleteArticle = asyncHandler(async (req, res) => {
-  const id = req.params?.id || req.body?.id;
+    const id = req.params?.id || req.body?.id;
 
-  if (!id) {
-    throw new ApiError(400, "Article id is required");
-  }
-
-  const article = await Articles.findByIdAndDelete(id);
-
-  if (!article) {
-    throw new ApiError(404, "Article not found");
-  }
-
-  // delete image
-  if(article.imageURL) {
-    const imageName = article.imageURL.split("/").pop().split(".")[0];
-    const isDeleted = await deleteImage(imageName);
-    if (!isDeleted) {
-      throw new ApiError(500, "Failed to delete image");
+    if (!id) {
+        throw new ApiError(400, "Article id is required");
     }
-  }
 
-  return res.status(200).json(new ApiResponse(200, "Article deleted successfully"));
+    const article = await Article.findByIdAndDelete(id);
+
+    if (!article) {
+        throw new ApiError(404, "Article not found");
+    }
+
+    // delete image
+    if (article.imageURL) {
+        const imageName = article.imageURL.split("/").pop().split(".")[0];
+        const isDeleted = await deleteImage(imageName);
+        if (!isDeleted) {
+            throw new ApiError(500, "Failed to delete image");
+        }
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, "Article deleted successfully"));
 });
 
-export { createArticle, getArticles, getArticleById, updateArticle, deleteArticle };
+export {
+    createArticle,
+    getArticles,
+    getArticleById,
+    likeArticle,
+    updateArticle,
+    deleteArticle,
+};
