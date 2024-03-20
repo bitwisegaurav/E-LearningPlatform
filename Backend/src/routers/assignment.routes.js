8adf80164883bf6729ea74bd57b913340c6450a7
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-20 21:43:29 +0530

    Add assignment routes

---
+++ Backend/src/routers/assignment.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { verifyUser } from "../middlewares/auth.middleware";
+import { verifyAdmin } from "../middlewares/adminAuth.middleware";
+import {
+    createAssignment,
+    deleteAssignment,
+    getAssignmentById,
+    getAssignments,
+    updateAssignment,
+} from "../controllers/assignment.controller";
+
+const router = Router();
+
+router.route("/health").get((__, res) => {
+    // This is a simple route that returns a response with a message and a status code.
+    const msg = "Server is up!";
+    const statusCode = 200;
+    return res.status(statusCode).json({ message: msg, status: statusCode });
+});
+
+router
+    .route("/create-assignment")
+    .post(verifyUser, verifyAdmin, createAssignment);
+router.route("/get-assignments").get(verifyUser, getAssignments);
+router.route("/get-assignmentById/:id").get(verifyUser, getAssignmentById);
+router.route("/update-assignment/:id").patch(verifyUser, verifyAdmin, updateAssignment);
+router.route("/delete-assignment/:id").delete(verifyUser, verifyAdmin, deleteAssignment);
+
+export default router;
