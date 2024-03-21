e957b85baea05ec0af79967000d2dee8d7c1603e
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-21 19:13:52 +0530

    Add subject routes

---
+++ Backend/src/routers/subjects.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import { verifyUser } from "../middlewares/auth.middleware.js";
+import { verifyAdmin } from "../middlewares/adminAuth.middleware.js";
+import {
+    createSubject,
+    deleteSubject,
+    getSubjectById,
+    getSubjects,
+    updateSubject,
+} from "../controllers/subject.controller.js";
+
+const router = Router();
+
+router.route("/create-subject").post(verifyUser, verifyAdmin, createSubject);
+router.route("/get-subjects").get(verifyUser, getSubjects);
+router.route("/get-subject/:id").get(verifyUser, getSubjectById);
+router.route("/update-subject").patch(verifyUser, verifyAdmin, updateSubject);
+router
+    .route("/delete-subject/:id")
+    .delete(verifyUser, verifyAdmin, deleteSubject);
+
+export default router;
