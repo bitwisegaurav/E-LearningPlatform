7a3982bb396455e8da0a6db4e88c268b116cf611
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-21 21:35:08 +0530

    Add degree and subject routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,6 +17,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // importing routes
+import imageRoutes from "./routers/image.routes.js";
 import userRoutes from "./routers/user.routes.js";
 import courseRoutes from "./routers/course.routes.js";
 import moduleRoutes from "./routers/module.routes.js";
@@ -24,7 +25,8 @@ import roadmapRoutes from "./routers/roadmap.routes.js";
 import followerRoutes from "./routers/follower.routes.js";
 import articleRoutes from "./routers/article.routes.js";
 import assignmentRoutes from "./routers/assignment.routes.js";
-import imageRoutes from "./routers/image.routes.js";
+import degreeRoutes from "./routers/degree.routes.js";
+import subjectRoutes from "./routers/subject.routes.js";
 
 // setting routes
 app.use("/api/v1/image", imageRoutes);
@@ -35,5 +37,7 @@ app.use("/api/v1/roadmap", roadmapRoutes);
 app.use("/api/v1/follower", followerRoutes);
 app.use("/api/v1/article", articleRoutes);
 app.use("/api/v1/assignment", assignmentRoutes);
+app.use("/api/v1/degree", degreeRoutes);
+app.use("/api/v1/subject", subjectRoutes);
 
 export { app };
