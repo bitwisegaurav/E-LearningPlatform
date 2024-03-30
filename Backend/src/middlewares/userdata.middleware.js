eb3b67bb325e8b50e521dd6813b30853f397b237
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-30 22:05:49 +0530

    Add userdata.middleware.js middleware

---
+++ Backend/src/middlewares/userdata.middleware.js
@@ -0,0 +1,61 @@
+import { options } from "../constants.js";
+import { generateAccessAndRefreshToken } from "../controllers/user.controller.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.util.js";
+import { asyncHandler } from "../utils/asyncHandler.util.js";
+import jwt from "jsonwebtoken";
+
+const getUser = asyncHandler(async (req, res, next) => {
+    try {
+        const accessToken =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!accessToken) {
+            const refreshToken = req.cookies?.refreshToken;
+            if (!refreshToken) {
+                console.log("Unauthorized access");
+                next();
+            }
+            const decodedValues = await jwt.verify(
+                refreshToken,
+                process.env.JWT_REFRESH_TOKEN_SECRET,
+            );
+            const user = await User.findById(decodedValues?._id).select([
+                "-password",
+            ]);
+            // refresh accessToken and refreshToken
+            const {
+                accessToken: newAccessToken,
+                refreshToken: newRefreshToken,
+            } = await generateAccessAndRefreshToken(user._id);
+
+            res.cookie("accessToken", newAccessToken, options);
+            res.cookie("refreshToken", newRefreshToken, options);
+            req.user = user;
+
+        } else {
+            const decodedValues = await jwt.verify(
+                accessToken,
+                process.env.ACCESS_TOKEN_SECRET,
+            );
+
+            const user = await User.findById(decodedValues?._id).select(
+                "-password -refreshToken",
+            );
+
+            if (!user) {
+                console.log("User not found");
+                next();
+            }
+
+            req.user = user;
+        }
+        next();
+    } catch (error) {
+        console.log(error?.message || "Invalid access token");
+        next();
+    }
+});
+
+export { getUser };
