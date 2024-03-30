e3a28710b1f8ba8949d733f48e922538b0cdec75
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-03-30 22:06:00 +0530

    Add getUser middleware to get-user route

--- Backend/src/routers/user.routes.js
+++ Backend/src/routers/user.routes.js
@@ -14,6 +14,7 @@ import {
 } from "../controllers/user.controller.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { getUser } from "../middlewares/userdata.middleware.js";
 
 const router = Router();
 
@@ -31,7 +32,7 @@ router.route("/register-user").post(
     registerUserProfile,
 );
 router.route("/get-user").get(verifyUser, getUserProfile);
-router.route("/get-user/:username").get(getUserProfileByUsername);
+router.route("/get-user/:username").get(getUser, getUserProfileByUsername);
 router.route("/update-user").patch(verifyUser, updateUserProfile);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyUser, logoutUser);
